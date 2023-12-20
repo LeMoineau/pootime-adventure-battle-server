75604472fdad6706898a9dd47155bc0217adc3f6
Pierre Faber <56643922+LeMoineau@users.noreply.github.com>
2023-12-20 12:28:37 +0000

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+dotenv.config();
+const port = process.env.PORT;
+
+const app: Express = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { cors: { ...cors() } });
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("coucou", () => {
+    console.log(`user #${socket.id} send coucou !`);
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
